386bae97b37cee3a6af48089357dae54c90fd388
ivans <ivansenik58@gmail.com>
2023-12-19 21:26:27 +0100

    adding the fe

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,3 +1,3 @@
 INSERT INTO users VALUES
 ('user1', 'Senyk Ivan', 'senikiva58@gmail.com', '12345678'),
-('user2', 'Chaban Mykhailo', 'cmisa569@gmail.com', '14589621');
+('user2', 'Chaban Mykhailo', 'cmisa695@gmail.com', '14589621');
