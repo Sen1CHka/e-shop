784bed5f842df4922b0ba1a9564749810028e763
--list <--list>
2023-12-25 11:49:31 +0100

    Add controllers

---
+++ src/main/java/cz/cvut/fit/tjv/contracts/User.java
@@ -0,0 +1,18 @@
+package cz.cvut.fit.tjv.contracts;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import cz.cvut.fit.tjv.domain.Order;
+import cz.cvut.fit.tjv.domain.Product;
+import cz.cvut.fit.tjv.domain.State;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+public class User {
+
+    private String username;
+    private String realName;
+    private String email;
+    private Collection<Order> orders;
+}
